9d1fa1969ecbf8dff8229e7f670f61be9bea7e06
Vijay Kumar <vjkumar1992@gmail.com>
2022-01-17 17:25:12 +0530

    revised

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,5 +1,8 @@
-﻿using OpenQA.Selenium.Firefox;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using System;
+using System.Threading;
 
 namespace ConsoleApp7
 {
@@ -7,13 +10,26 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            
+            IWebDriver driver = null;
             Console.WriteLine("Please enter the browser in which you want to test");
-           string browser = Console.ReadLine();
+            string browser = Console.ReadLine();
+            browser = browser.ToLower();
             switch (browser)
             {
-                case "Firefox":
-                    FirefoxDriver Driver = new FirefoxDriver();
+                case "firefox":
+                    driver = new FirefoxDriver();
+
+                    driver.Url = "https://demo.creativeitem.com/bayanno/login";
+
+                    break;
+                case "chrome":
+                    driver = new ChromeDriver();
+                    driver.Url = "https://demo.creativeitem.com/bayanno/login";
+                    break;
+                case "IE":
+                    driver.Url = "https://demo.creativeitem.com/bayanno/login";
+                    break;
+
             }
         }
     }
